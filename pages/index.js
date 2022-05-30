17529818f897ea219d72720157ceae9603323c84
Viktor Holovin <holovin@mail.com>
2022-05-30 17:56:31 +0200

    First commit

--- pages/index.js
+++ pages/index.js
@@ -13,55 +13,54 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Viktor Holovin
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+	  Web developer
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="https://blog.holovin.com/" className={styles.card}>
+            <h2>Blog</h2>
+            <p>Coding</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
+            href="https://facebook.com/holovin/"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2>Facebook</h2>
+            <p>Social</p>
+          </a>
+          <a href="https://github.com/holovin777" className={styles.card}>
+            <h2>GitHub</h2>
+            <p>Developing</p>
+          </a>
+
+          <a href="https://instagram.com/holovin777" className={styles.card}>
+            <h2>Instagram</h2>
+            <p>Photos</p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://facebook.com/holovin/"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h2>Facebook</h2>
+            <p>Social</p>
           </a>
+
         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://holovin.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+	  Viktor Holovin
         </a>
       </footer>
     </div>
